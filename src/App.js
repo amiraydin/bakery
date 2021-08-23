11fa90b726fd4ce2ede4103e2a10033542d6ea33
amiraydin <amiraydin@outlook.fr>
2021-08-23 17:27:49 +0200

    on secend step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Pay from "./compenants/Pay";
+import Add from "./compenants/Add";
+import List from "./compenants/List";
+import Button from "./compenants/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import * as yup from 'yup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: "add",
+      items: []
+    };
+  }
+
+  selectAdd = (e) => {
+    console.log("App.js selectAdd e.target.value", e.target.value);
+    this.setState({ activeTab: e.target.value });
+  };
+  selectList = (e) => {
+    console.log(e.target.value);
+  };
+  selectPay = (e) => {
+    console.log(e.target.value);
+  };
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Button onChange={this.selectAdd} >Add</Button>
+          <Button onChange={this.selectList}>List</Button>
+          <Button onChange={this.selectPay}>Pay</Button>
+
+          <Add />
+          <List />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/compenants/Add.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Add extends React.Component {
+  render() {
+    return <div>Add</div>;
+  }
+}
+
+export default Add;
